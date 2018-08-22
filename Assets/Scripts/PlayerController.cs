2b6f8467553cf88c18fa0eb90fb6fa190d416ba2
Rorazoro <rorazoro@gmail.com>
2018-08-22 13:02:15 -0400

    Created PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+	[SerializeField]
+	private float speed = 5f;
+
+	private PlayerMotor motor;
+
+	public void Start() {
+		motor = GetComponent<PlayerMotor>();
+	}
+
+	public void Update() {
+		float _xMov = Input.GetAxisRaw("Horizontal");
+		float _zMov = Input.GetAxisRaw("Vertical");
+
+		Vector3 _movHorizontal = transform.right * _xMov;
+		Vector3 _movVertical = transform.forward * _zMov;
+
+		Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+		motor.Move(_velocity);
+	}
+}
