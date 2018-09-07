62690034f4decadada4a1f353293d0a1218784a0
Rorazoro <rorazoro@gmail.com>
2018-09-07 13:30:40 -0400

    Added RegisterPlayer method to GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,5 +5,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 	private static Dictionary<string, Player>  players = new Dictionary<string, Player>();
 
-	
+	public void RegisterPlayer (string netID, Player player) {
+		string playerID = "Player " + netID;
+		players.Add(playerID, player);
+		player.transform.name = playerID;
+	}
 }
