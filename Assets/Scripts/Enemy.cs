4cbadd456c2858f0a55c8dc180bf1bded63451e4
Rorazoro <rorazoro@gmail.com>
2018-09-14 13:20:09 -0400

    Added EnemySpawn and Modified Enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Networking;
+
+public class Enemy : NetworkBehaviour {
+	[SerializeField]
+	private int maxHealth = 100;
+	[SerializeField][SyncVar] 
+	private int currentHealth;
+
+	[SerializeField]
+	private Transform Target;
+
+	private NavMeshAgent Nav;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		Target = GameObject.FindGameObjectWithTag("Player") != null ? GameObject.FindGameObjectWithTag("Player").transform : null;
+		Nav = GetComponent<NavMeshAgent>();
+	}
+
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update()
+	{
+		if (Target != null) {
+			Nav.SetDestination(Target.position);
+		}
+	}
+}
