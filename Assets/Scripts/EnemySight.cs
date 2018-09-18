c1f6b470824c6029df2f7fe33f704b015a109c42
Rorazoro <rorazoro@gmail.com>
2018-09-18 13:03:08 -0400

    Created Enemy Sight Script

---
+++ Assets/Scripts/EnemySight.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemySight : MonoBehaviour {
+	[SerializeField]
+	public float fieldOfViewAngle = 110f;
+	[SerializeField]
+	public bool playerInSight;
+	[SerializeField]
+	public Vector3 playerLastSighting;
+
+	private NavMeshAgent nav;
+	private SphereCollider col;
+	private GameObject player;
+	private Vector3 previousSighting;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		nav = GetComponent<NavMeshAgent>();
+		col = GetComponent("");
+	}
+
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update()
+	{
+		
+	}
+
+	/// <summary>
+	/// OnTriggerStay is called once per frame for every Collider other
+	/// that is touching the trigger.
+	/// </summary>
+	/// <param name="other">The other Collider involved in this collision.</param>
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			playerInSight = false;
+			Vector3 direction = other.transform.position - transform.position;
+			float angle = Vector3.Angle(direction, transform.forward);
+
+			if (angle < fieldOfViewAngle * 0.5f) {
+				RaycastHit hit;
+
+				if(Physics.Raycast(transform.position + transform.up, direction.normalized, out hit, col.radius)) {
+					if (hit.collider.gameObject.tag == "Player") {
+						playerInSight = true;
+					}
+				}
+			}
+		}
+	}
+}
