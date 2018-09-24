e46491edd926eb7a4755fa93f55abb89288dad84
Rorazoro <rorazoro@gmail.com>
2018-09-24 13:03:45 -0400

    Added Enemy view angle and radius and added FieldOfViewEditor

---
+++ Assets/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(EnemySight))]
+public class FieldOfViewEditor : Editor {
+
+	private void OnSceneGUI() {
+		EnemySight sight = (EnemySight)target;
+		Handles.color = Color.white;
+		Handles.DrawWireArc(sight.transform.position, Vector3.up, Vector3.forward, 360, sight.viewRadius);
+		Vector3 viewingAngleA = sight.DirFromAngle(-sight.viewAngle / 2, false);
+		Vector3 viewingAngleB = sight.DirFromAngle(sight.viewAngle / 2, false);
+
+		Handles.DrawLine(sight.transform.position, sight.transform.position + viewingAngleA * sight.viewRadius);
+		Handles.DrawLine(sight.transform.position, sight.transform.position + viewingAngleB * sight.viewRadius);
+
+		Handles.color = Color.red;
+		foreach (Transform visibleTarget in sight.visibleTargets)
+		{
+			Handles.DrawLine(sight.transform.position, visibleTarget.position);
+		}
+	}
+
+}
--- Assets/Scripts/EnemySight.cs
+++ Assets/Scripts/EnemySight.cs
@@ -11,6 +11,41 @@ public class EnemySight : MonoBehaviour {
 	public LayerMask targetMask;
 	public LayerMask obstacleMask;
 
+	public List<Transform> visibleTargets = new List<Transform>();
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+		StartCoroutine("FindTargetsWithDelay", .2f);
+	}
+
+	IEnumerator FindTargetsWithDelay(float delay){
+		while(true){
+			yield return new WaitForSeconds(delay);
+			FindVisibleTargets();
+		}
+	}
+
+	private void FindVisibleTargets() {
+		visibleTargets.Clear();
+		Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+		foreach(Collider col in targetsInViewRadius) {
+			Transform target = col.transform;
+			Vector3 dirToTarget = (target.position - transform.position).normalized;
+			if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2) {
+				float distToTarget = Vector3.Distance(transform.position, target.position);
+
+				if (!Physics.Raycast(transform.position, dirToTarget, distToTarget, obstacleMask)) {
+
+				}
+			}
+		}
+	}
+
 	public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal) {
 		if (!angleIsGlobal) {
 			angleInDegrees += transform.eulerAngles.y;
